781692a75eb270a02c41c28c1a1efb1bff872246
shane <shanejlester@gmail.com>
2019-03-09 09:26:51 -0800

    Add friendship queries

--- src/db-helper-methods/neo4j.py
+++ src/db-helper-methods/neo4j.py
@@ -2,6 +2,8 @@
 # only be accessed via helper methods so the schema
 # can not be corrupted with strange data
 
+from datetime import datetime
+
 #from py2neo import Graph
 
 # constants for entity types
@@ -11,12 +13,16 @@ project = ("Project", "project_name")
 skill = ("Skill", "skill_name")
 interest = ("Interest", "interest_name")
 
+# constants for labels:
+friendship = ("is_friends_with", "added_on")
+
 
 #graph = Graph()
 
 # Getter methods for nodes
 
 def find_node(label, node_name):
+    # TODO: Would like a reason for failure here
     node_type = None
     if label is user[0]:
         node_type = user
@@ -37,6 +43,7 @@ def find_node(label, node_name):
 # Create methods for nodes
 
 def create_node(label, node_name):
+    # TODO: Would like a boolean here indicating whether it was successful or not
     node_type = None
     if label is user[0]:
         node_type = user
@@ -58,6 +65,7 @@ def create_node(label, node_name):
 
 
 def delete_node(label, node_name):
+    # TODO: Would like a boolean here indicating whether it was successful or not and give info back
     node_type = None
     if label is user[0]:
         node_type = user
@@ -72,30 +80,35 @@ def delete_node(label, node_name):
     else:
         errorMessage = "Incorrect label used when trying to delete node: {0} with name: {1}".format(label, node_name)
         raise ValueError(errorMessage)
-    query = "MATCH (u: {0}) WHERE  u.{1} = '{2}' DELETE u".format(node_type[0], node_type[1], node_name );
+    query = "MATCH (u: {0}) WHERE  u.{1} = '{2}' DETACH DELETE u".format(node_type[0], node_type[1], node_name );
     return query
 
+# Methods for relationships
+
+# Friendships between users
+def create_friendship(user1, user2):
+    # TODO: Would like a boolean here indicating whether it was created and returning the nodes/ rel
+    query = """MATCH (u1: {0} {{ {1}: '{2}' }}), (u2: {0} {{ {1}: '{3}' }})
+                MERGE(u1) - [ :{4} {{ {5} : '{6}' }}] -> (u2)
+                MERGE (u1) <- [:{4} {{ {5}: '{6}' }}] - (u2)
+                RETURN u1, u2""".format(user[0], user[1], user1, user2, friendship[0], friendship[1], str(datetime.now()) )
+    return query
+
+def delete_friendship(user1, user2):
+    # TODO: Would like some indication of what happened to caller returned here
+    query = """MATCH (u1: {0} {{ {1}: '{2}' }}) -[f: {4}] - (u2: {0} {{ {1}: '{3}' }} )
+                DELETE f""".format(user[0], user[1], user1, user2, friendship[0] )
+    return query
+
+
+
 if __name__ == '__main__':
-    #print(create_user("Shane"))
-    #print(create_skill("Cloud Computing"))
-   # print(create_interest("Bird Watching"))
-   # print(create_project("Expansion_DB"))
-   # print(create_organization("Bloomberg")) 
-    #print(find_user("Shane"))
-    print("New method")
-    print(delete_node("User", "Shane"))
-    print(delete_node("Skill", "Cloud Computing"))
-    print(delete_node("Interest", "Bird Watching"))
-    print(delete_node("Project", "Expansion_DB"))
-    print(delete_node("Organization", "Bloomberg"))
-    print("Old method")
-    print(delete_user("Shane"))
-    print(delete_skill("Cloud Computing"))
-    print(delete_interest("Bird Watching"))
-    print(delete_project("Expansion_DB"))
-    print(delete_organization("Bloomberg"))
-   # print(delete_user("Shane")) 
-   # print(delete_skill("Cloud Computing"))
-   # print(delete_interest("Bird Watching"))
-   # print(delete_project("Expansion_DB"))
-   # print(delete_organization("Bloomberg"))
+    print(create_friendship('Jessica', 'Amanda'))
+    print(delete_friendship('Jessica','Amanda'))
+
+   # print("New method")
+    #print(delete_node("User", "Shane"))
+    #print(delete_node("Skill", "Cloud Computing"))
+   # print(delete_node("Interest", "Bird Watching"))
+   # print(delete_node("Project", "Expansion_DB"))
+   # print(delete_node("Organization", "Bloomberg"))

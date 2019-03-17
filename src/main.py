22a23135e62e2a1e66125f49ca0da976d5be0ebf
shane <shanejlester@gmail.com>
2019-03-16 19:09:11 -0700

    Add switch to load multiple data files at once from yaml file

--- src/main.py
+++ src/main.py
@@ -1,20 +1,35 @@
 from plumbum import (cli, local)
 from datetime import datetime
+from collections import OrderedDict
 import pandas as pd
-
+import yaml
 
 
 class CollaboratorDotNet(cli.Application):
-    #_read_data = cli.SwitchAttr(
-    #    ['--input-data', , mandatory=False, argtype=cli.ExistingFile, 
-    #    help="
-    #)
-
+    # Types of acceptable files at command line
     _valid_file_types = ('skill', 'project', 'organization', 'interest', 'distance', 'user')
-
+    # Structure of csv columns
     _valid_file_structures = {
-        'skill': ['User_Id', 'Skill ', 'Skill level']
+            _valid_file_types[0] : OrderedDict({'User_Id': int, 'Skill ': str, 'Skill level': int}),
+            _valid_file_types[1] : OrderedDict({'User_id': int, 'Project': str}),
+            _valid_file_types[2] : OrderedDict({'User_id': int, 'organization': str, 'organization type': str}),
+            _valid_file_types[3] : OrderedDict({'User_id': int, 'Interest': str, 'Interest level': int}),
+            _valid_file_types[4] : OrderedDict({"Organization 1": str, "Organization 2": str, "Distance": int}),
+            _valid_file_types[5] : OrderedDict({'User_id': int, 'First name': str, 'Last name': str})
     }
+
+    @cli.switch(['-m', '--input-many'], str)
+    def input_many_data(self, file_path_to_yaml):
+        """ Input a list of csv files to input input_data """
+        yaml_file = None
+        with open(file_path_to_yaml) as raw_config_file:
+            try:
+                yaml_file  = yaml.load(raw_config_file, yaml.FullLoader)
+            except yaml.YAMLError as err:
+                raise err
+        for i in yaml_file['load_all_data']:
+            self.input_data(i)
+        
 
     # will input a file named file_name that is input data of file_type
     @cli.switch(['-i', '--input'], str)
@@ -27,8 +42,27 @@ class CollaboratorDotNet(cli.Application):
         self.is_csv(file_path)
         self.is_valid_file_type(file_type, self._valid_file_types, self._valid_file_structures, file_path)
         print('Input validated at ... {0}\n'.format(datetime.now()))
-        print('Attempting to read data from {0} at ... {1}'.format(file_path, datetime.now()))
-        
+        print('Attempting to read data from {0} at ... {1}'.format(file_path, datetime.now())) 
+        input_data = self.read_data(file_path, file_type)
+        print('Data read and cleaned at ... {0}'.format(datetime.now()))
+        print(input_data)
+        print()
+        # TODO: Load the data according to the appropriate file
+    def read_data(self, file_path, file_type):
+        data_to_load = pd.read_csv(file_path)
+        #print(data_to_load, data_to_load.dtypes)
+        #print()
+        #for (key, value) in self._valid_file_structures[file_type].items():
+        #    print(data_to_load[key].astype(value))
+        types_to_convert = []
+        for (key, value) in self._valid_file_structures[file_type].items():
+            if value ==  int:
+                types_to_convert.append(key)
+        for data_type in types_to_convert:
+            data_to_load[data_type] = pd.to_numeric(data_to_load[data_type], errors='coerce')
+            data_to_load = data_to_load.dropna()
+            data_to_load[data_type] = data_to_load[data_type].astype(int)
+        return data_to_load
 
     def main(self):
         #print('Command line application starting...')
@@ -54,7 +88,7 @@ class CollaboratorDotNet(cli.Application):
             err_message = 'Invalid file input type "{0}", exiting'.format(file_type)
             raise TypeError(err_message)
         headers = list(pd.read_csv(file_path).columns)
-        if file_structure[file_type] != headers:
+        if list(file_structure[file_type].keys()) != headers:
             print("Failed to match valid proper csv data structure  at ... {0}".format(datetime.now()))
             err_message = 'Invalid file structure at "{0}", exiting'.format(file_path)
             raise TypeError(err_message)
